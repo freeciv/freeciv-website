ce1582ce267ae53800d0c6f93be809ed1bc9c927
Jacob Nevins <0jacobnk.fcdev@chiark.greenend.org.uk>
2015-10-04 15:06:10 +0000

    Pre-deployment website changes.
     - restore some links lost in translation from wiki
     - add commented-out placeholders for elements we'll need at release time
     - tweak wording on index.html navbar
     - demote Wiki link in toolbar
     - disable Javascript which guesses which download link to offer for now,
       as the resulting page will be misleading when the latest version is not
       yet available for Windows
     - minor formatting/link tidyup
    
    [[originally from svn r30013]]

--- js/freeciv.js
+++ js/freeciv.js
@@ -7,25 +7,33 @@ $(function() {
   window.location="download.html";
   });
   $("#web_box").click(function() {
-  window.location="https://play.freeciv.org";
+  window.location="https://play.freeciv.org/";
   });
   $("#forum_box").click(function() {
-  window.location="http://forum.freeciv.org";
+  window.location="http://forum.freeciv.org/";
   });
   $("#development_box").click(function() {
-  window.location="http://freeciv.wikia.com/wiki/How_to_Contribute";
+  window.location="http://www.freeciv.org/wiki/How_to_Contribute";
   });
 
   /* show download link based on the user's operating system */
+  /* FIXME: commented out until we can arrange that the displayed version
+   * number is correct even when e.g. the Windows package is not yet
+   * up to date. Would also be good to identify the platform we've guessed
+   * to the user and provide a backup link to download.html in the same
+   * place.
+  
   if (window.ui.os=="Windows") {
     $("#download_button").attr("href", "http://prdownloads.sourceforge.net/freeciv/Freeciv-2.5.1-win32-gtk2-setup.exe?download");
   } else if (window.ui.os=="Linux") {
     $("#download_button").attr("href", "http://prdownloads.sourceforge.net/freeciv/freeciv-2.5.1.tar.bz2?download");
   } else if (window.ui.os=="Mac OS X") {
-    $("#download_button").attr("href", "http://freeciv.wikia.com/wiki/Install-MacOSX");
+    $("#download_button").attr("href", "http://www.freeciv.org/wiki/Install-MacOSX");
   } else {
     $("#download_button").attr("href", "download.html");
   }
+
+   * end of FIXME */
 
 });
 
